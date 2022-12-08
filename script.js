a088b42f9b86b41e15951ad64c6a70762412c41c
daniel <daniel@dondisfraz.com>
2022-12-08 11:34:22 +0000

    Fix

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+alert("Funciona?");
+const pokemon = ["charmander", "bulbasaur", "squirtle"];
+console.log("El array");
+console.log(pokemon);
 let i = 2;
-let len = cars.length;
+let len = pokemon.length;
 let text = "";
 for (; i < len; i++) {
-  text += cars[i] + "<br>";
+    alert(i);
+    console(pokemon[i]);
+    console(i);
+  text += pokemon[i] + "<br>";
 }
